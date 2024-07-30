4151a810b8719edddc9c4fc2714c0247cbb98746
babupakkakivellu <149465835+babupakkakivellu@users.noreply.github.com>
2024-07-30 10:29:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
-
-
 API_ID = '27476108'  # Replace with your actual API ID
 API_HASH = 'ed25a3c999b0849364a83b11bea390a0'  # Replace with your actual API hash
 BOT_TOKEN = '7244115020:AAE-agqz28x9OcyoIzbpHznaB2hLlmk1ihQ'  # Replace with your actual Bot Token
-BOT_USERNAME = "sdencoderrbot"
+
