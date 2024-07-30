093e0998f89e9917e322ff91bcb4dc0b14c750d1
babupakkakivellu <149465835+babupakkakivellu@users.noreply.github.com>
2024-07-30 11:10:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,8 @@ async def start(client, message: Message):
         "You can use the following commands:\n"
         "/merge - Merge multiple videos.\n"
         "Send videos one by one, and I will ask for the output filename once all are received.\n"
-        "Type /done when finished sending files."
+        "Type /done when finished sending files.\n"
+        "Use /set_thumbnail to set a thumbnail image."
     )
     await message.reply(welcome_text, reply_markup=keyboard)
 
@@ -35,6 +36,7 @@ async def help_command(client, message: Message):
         "/merge - Start merging videos.\n"
         "Send videos one by one and type /done when finished.\n"
         "Provide a filename for the merged video.\n"
+        "/set_thumbnail - Send an image to set it as the video thumbnail.\n"
         "You can also use inline buttons for quick actions."
     )
     await message.reply(help_text)
@@ -42,11 +44,12 @@ async def help_command(client, message: Message):
 @app.on_message(filters.command("settings") & filters.private)
 async def settings_command(client, message: Message):
     user_id = message.from_user.id
-    settings = user_settings.get(user_id, {"upload_format": "video", "caption_font": "default"})
+    settings = user_settings.get(user_id, {"upload_format": "video", "caption_font": "default", "thumbnail": None})
     settings_text = (
         f"Current settings:\n"
         f"Upload format: {settings['upload_format']}\n"
-        f"Caption font: {settings['caption_font']}\n\n"
+        f"Caption font: {settings['caption_font']}\n"
+        f"Thumbnail: {'Set' if settings['thumbnail'] else 'Not Set'}\n\n"
         "To change settings, send /set_format <format> or /set_font <font>."
     )
     await message.reply(settings_text)
@@ -56,7 +59,7 @@ async def set_format(client, message: Message):
     user_id = message.from_user.id
     try:
         format = message.text.split(maxsplit=1)[1].strip()
-        user_settings[user_id] = user_settings.get(user_id, {"upload_format": "video", "caption_font": "default"})
+        user_settings[user_id] = user_settings.get(user_id, {"upload_format": "video", "caption_font": "default", "thumbnail": None})
         user_settings[user_id]["upload_format"] = format
         await message.reply(f"Upload format set to {format}.")
     except IndexError:
@@ -67,11 +70,23 @@ async def set_font(client, message: Message):
     user_id = message.from_user.id
     try:
         font = message.text.split(maxsplit=1)[1].strip()
-        user_settings[user_id] = user_settings.get(user_id, {"upload_format": "video", "caption_font": "default"})
+        user_settings[user_id] = user_settings.get(user_id, {"upload_format": "video", "caption_font": "default", "thumbnail": None})
         user_settings[user_id]["caption_font"] = font
         await message.reply(f"Caption font set to {font}.")
     except IndexError:
         await message.reply("Please specify the font.")
+
+@app.on_message(filters.command("set_thumbnail") & filters.private)
+async def set_thumbnail_command(client, message: Message):
+    await message.reply("Please send the image you want to use as a thumbnail.")
+
+@app.on_message(filters.photo & filters.private)
+async def receive_thumbnail(client, message: Message):
+    user_id = message.from_user.id
+    thumbnail_file = await client.download_media(message.photo.file_id)
+    user_settings[user_id] = user_settings.get(user_id, {"upload_format": "video", "caption_font": "default", "thumbnail": None})
+    user_settings[user_id]["thumbnail"] = thumbnail_file
+    await message.reply("Thumbnail set successfully.")
 
 @app.on_message(filters.video & filters.private)
 async def receive_videos(client, message: Message):
@@ -86,15 +101,18 @@ async def receive_videos(client, message: Message):
         }
     
     pending_files[user_id]["videos"].append(video_file)
+    video_count = len(pending_files[user_id]["videos"])
+
+    await message.reply(f"Received file {video_count}.")
 
     if pending_files[user_id]["expected_count"] is not None:
-        if len(pending_files[user_id]["videos"]) < pending_files[user_id]["expected_count"]:
-            remaining_files = pending_files[user_id]["expected_count"] - len(pending_files[user_id]["videos"])
-            await message.reply(f"Received file. You need to send {remaining_files} more files.")
+        if video_count < pending_files[user_id]["expected_count"]:
+            remaining_files = pending_files[user_id]["expected_count"] - video_count
+            await message.reply(f"You need to send {remaining_files} more files.")
         else:
             await message.reply("All files received. Please provide the output filename (e.g., merged_video.mp4):")
     else:
-        await message.reply("File received. Send more files or type /done when finished.")
+        await message.reply("Send more files or type /done when finished.")
 
 @app.on_message(filters.text & filters.private)
 async def handle_text_messages(client, message: Message):
@@ -116,6 +134,7 @@ async def handle_text_messages(client, message: Message):
 
             video_files = pending_files[user_id]["videos"]
             output_file = filename
+            thumbnail = user_settings[user_id].get("thumbnail")
             
             await message.reply("Merging videos, please wait...")
 
@@ -126,12 +145,14 @@ async def handle_text_messages(client, message: Message):
             caption = "Videos merged successfully!"
 
             if settings["upload_format"] == "video":
-                await client.send_video(message.chat.id, video=output_file, caption=caption)
+                await client.send_video(message.chat.id, video=output_file, caption=caption, thumb=thumbnail)
             else:
-                await client.send_document(message.chat.id, document=output_file, caption=caption)
+                await client.send_document(message.chat.id, document=output_file, caption=caption, thumb=thumbnail)
 
             for file in video_files:
                 os.remove(file)
+            if thumbnail:
+                os.remove(thumbnail)
             os.remove(output_file)
             
             del pending_files[user_id]
