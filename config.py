b84e2b6e2dc5ba9e480af50ce8c4051da00c15bf
babupakkakivellu <149465835+babupakkakivellu@users.noreply.github.com>
2024-08-01 08:27:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-api_id = '27476108'  # Replace with your actual AP
-api_hash = 'ed25a3c999b0849364a83b11bea390a0'  # Replace with your actual API hash
-bot_token = '7244115020:AAE-agqz28x9OcyoIzbpHznaB2hLlmk1ihQ'  # Replace with your actual Bot Token
+BASE_DIRECTORY = '/path/to/your/files'  # Update this to your actual directory
+API_ID = '27476108'  # Replace with your actual AP
+API_HASH = 'ed25a3c999b0849364a83b11bea390a0'  # Replace with your actual API hash
+BOT_TOKEN = '7244115020:AAE-agqz28x9OcyoIzbpHznaB2hLlmk1ihQ'  # Replace with your actual Bot Token
 
 allowed_users = {5422016608}  # Replace with actual user IDs
 
